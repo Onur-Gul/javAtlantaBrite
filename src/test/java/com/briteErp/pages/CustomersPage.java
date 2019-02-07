685d0487bae935cfee51e3bf64f166f886f82e19
Onur Gul <imonurgul@gmail.com>
2019-02-07 03:24:02 -0500

    Two more test cases automated in CRMManagerCustomersTest

--- src/test/java/com/briteErp/pages/CustomersPage.java
+++ src/test/java/com/briteErp/pages/CustomersPage.java
@@ -1,5 +1,6 @@
 package com.briteErp.pages;
 
+import com.briteErp.utilities.ConfigurationReader;
 import com.briteErp.utilities.Driver;
 import com.briteErp.utilities.TestBase;
 import org.openqa.selenium.WebElement;
@@ -12,9 +13,6 @@ public class CustomersPage extends TestBase {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-
-    @FindBy(tagName = "Create")
-    public WebElement createButton;
 
     @FindBy(xpath = "//a[@class='list-group-item'][2]")
     public WebElement BriteErpDemo ;
@@ -37,10 +35,27 @@ public class CustomersPage extends TestBase {
     @FindBy(xpath = "//div[@class='o_form_buttons_edit']/button")
     public WebElement saveButton;
 
+    @FindBy(xpath = "//div[@class='o_form_buttons_edit']/button[2]")
+    public WebElement discardButton;
+
     @FindBy(xpath = "(//strong[@class='o_kanban_record_title oe_partner_heading']/span)[1]")
     public WebElement customerNameOnCart;
 
     @FindBy(xpath = "//span[@class='o_field_char o_field_widget o_required_modifier']")
     public WebElement customerNameOnAccount;
+
+    @FindBy(className = "modal-body")
+    public WebElement warningPopUp;
+
+    @FindBy(xpath = "//div[@class='modal-footer']/button[1]")
+    public WebElement warningOkButton;
+
+    @FindBy(xpath = "//button[@aria-label='kanban']")
+    public WebElement kanbanViewButton;
+
+    @FindBy(xpath = "//button[@aria-label='list']")
+    public WebElement listViewButton;
+
+
 
 }
--- src/test/java/com/briteErp/tests/FunctionalTests/CRMManagerCustomersTest.java
+++ src/test/java/com/briteErp/tests/FunctionalTests/CRMManagerCustomersTest.java
@@ -56,5 +56,42 @@ public class CRMManagerCustomersTest extends TestBase {
     }
 
 
+    @Test
+    public void BRIT_3161() {
+        Driver.getDriver().get("http://52.39.162.23/web/login");
+        pages.getCustomersPage().BriteErpDemo.click();
+        pages.login().login(ConfigurationReader.getProperty("CRMManagerEmail") , ConfigurationReader.getProperty("CRMManagerPassword") );
+        pages.getCustomersPage().CRM1.click();
+        pages.getCustomersPage().Customers.click();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+        wait.until(ExpectedConditions.titleIs("Customers - Odoo"));
+        pages.getCustomersPage().Create.click();
+        pages.getCustomersPage().nameField.clear();
+        pages.getCustomersPage().nameField.sendKeys("Sample NameXYZ");
+        pages.getCustomersPage().discardButton.click();
+        String warningText = pages.getCustomersPage().warningPopUp.getText();
+        Assert.assertTrue(warningText.contains("Do you want to proceed?"));
+        pages.getCustomersPage().warningOkButton.click();
+        Assert.assertEquals(pages.getCustomersPage().CustomersHeader.getText(), "Customers");
+    }
+
+    @Test
+    public void BRIT_3183() {
+        extentLogger = report.createTest("CRM Manager should be able to see current customer cards on the Customers page)");
+        getCRMModule();
+        extentLogger.info("Navigating to the Customers page");
+        pages.getCustomersPage().Customers.click();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+        wait.until(ExpectedConditions.titleIs("Customers - Odoo"));
+        extentLogger.info("Verify that Kanban button is selected as default");
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("view_type=kanban&model"));
+        extentLogger.info("Verify that list button switches the page to list view");
+        pages.getCustomersPage().listViewButton.click();
+        WebDriverWait waitX = new WebDriverWait(Driver.getDriver(), 10);
+        waitX.until(ExpectedConditions.urlContains("view_type=list&model"));
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("view_type=list&model"));
+    }
+
+
 
 }
